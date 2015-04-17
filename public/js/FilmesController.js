1ceda57a8414b63894ffedaba4958f0736b29d02
brunobc <brunobezerrachaves@gmail.com>
2015-04-16 23:37:58 -0300

    Aula 4

--- public/js/FilmesController.js
+++ public/js/FilmesController.js
@@ -1,12 +1,35 @@
 function FilmesController($http, $scope) {
   $http.get('/lista')
   .success(function(retorno){
-    $scope.filme = retorno;
+    $scope.filmes = retorno.filmes;
   });
 
-  $http.post('/grava', { titulo: 'Gattaca', diretor:  'Andrew Nicool', ano: 1997 })
-  .success(function(retorno) {
-    console.log(retorno);
-  });
+  function Filme() {
+    this.titulo = '';
+    this.diretor = '';
+    this.ano = '';
+  }
+
+  $scope.filme = new Filme();
+
+  $scope.adicionaFilme = function() {
+    $http.post('/grava', $scope.filme)
+    .success(function(retorno) {
+      $scope.filmes.push(retorno);
+      $scope.filme = new Filme();
+    });
+  };
+
+  $scope.mostraFilme= function(filme) {
+    $scope.filmeSelecionado = filme;
+  };
+
+  $scope.deletaFilme = function(filme) {
+    $http.delete('/filme/' + filme._id).success(function(err, retorno) {
+      $scope.filmeSelecionado = null;
+      var index = $scope.filmes.indexOf(filme);
+      $scope.filmes.splice(index, 1);
+    });
+  };
 
 }
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,27 @@
+var Filme = require('../models/filme');
+
 exports.index = function(req, res) {
   res.render('index');
 }
 
 exports.lista = function(req, res) {
-  res.json({
-    titulo: 'Gangues de Nova Iorque',
-    diretor: 'Martin Scorsese',
-    ano: 2002
+  Filme.find({}, function(err, filmes) {
+    if (err) return console.log(err);
+    res.json({filmes: filmes});
   });
 };
 
 exports.grava = function(req, res) {
-  var filme = req.body;
-  console.log(filme);
-  res.send('Filme ' + filme.titulo  + ' recebido no servidor.');
+  var filme = new Filme(req.body);
+  filme.save(function(err, filme) {
+    if(err) return console.log(err);
+    res.send(filme);
+  });
+};
+
+exports.deleta = function(req, res) {
+  var id = req.params.id;
+  Filme.findByIdAndRemove(id, function(err, filme) {
+    res.send('Filme ' + filme.titulo + ' removido com sucesso');
+  });
 };
